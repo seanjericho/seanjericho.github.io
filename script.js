b82635449246e2ccff88e728bf4fcae0923ebc8a
seanjericho <seanjericho8@gmail.com>
2026-06-01 22:48:35 +0700

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Galaxy of love From Jojooo',//untuk deskripsi planet layer 1
+  "I love younso much",//untuk deskripsi planet layer 2
+  "Semangat yaa sayangg♡",//untuk deskripsi planet layer 3
+  "01/06/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
